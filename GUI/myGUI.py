3ecaf7c48dbac6ca6be2f95c0d9f083ed58d5fb2
NoVaci <novaci.merc@gmail.com>
2017-06-08 07:53:08 +0700

    new layout: add listbox for searching, detail info on left panel

--- GUI/myGUI.py
+++ GUI/myGUI.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from Tool import Tool
+from Tools.Tool import Tool
 import threading
 
 class myGUI():
@@ -9,7 +9,12 @@ class myGUI():
         self.tool = Tool()
         self.dirPath = (u"H:/Doujin-Manga/test/")
         self.pathVar = StringVar()
+        self.nameVar = StringVar()
         self.threads = []
+
+        self.dbName  = 'mainDB.txt'
+
+        # self.db      = self.tool.readDB(self.dirPath + self.dbName)
 
     def initializeGUI(self, parent):
         frameInput = Frame(parent)  # Test: input + button in 1 frame
@@ -18,34 +23,51 @@ class myGUI():
         # ----------------------------------------------------
         # Top-left: path input
         # ----------------------------------------------------
-        # Label(frameInput, text = 'Path', fg = 'red').grid(column = 0, row = 0, sticky = NW)
-        Label(frameInput, text = 'Path', fg = 'red').pack(side = LEFT)
+        Label(frameInput, text = 'Path', fg = 'red').grid(column = 0, row = 0, sticky = NW)
+        # Label(frameInput, text = 'Path', fg = 'red').pack(side = LEFT)
         pathEntry = Entry(frameInput, textvariable = self.pathVar)
-        # pathEntry.grid(column = 0, row = 0, sticky = W)
-        pathEntry.pack(side = LEFT, padx = 10)
+        pathEntry.grid(column = 1, row = 0, columnspan = 2, sticky = NW)
+        # pathEntry.pack(side = LEFT, padx = 10)
         self.pathVar.set(u"%s" % self.dirPath)
+
+        # Seach box
+        Button(frameInput, text = 'Search', fg = 'blue', command = lambda: self.searchAndShow(self.nameVar.get())) \
+                    .grid(column = 0, row = 1, sticky = NW)
+        nameSearch = Entry(frameInput, textvariable = self.nameVar, width = 30)
+        nameSearch.grid(column = 0, row = 2, columnspan = 3, sticky = NW, padx = 2)
+
+        # Detail Info show
+        Label(frameInput, text = 'First Entry--------------', fg = 'blue').grid(column = 0, row = 3, pady = 20, sticky = W)
+        Label(frameInput, text = 'Language:').grid(column = 0, row = 4, sticky = W)
+        Label(frameInput, text = 'Size:').grid(column = 0, row = 5, sticky = W)
 
         # ----------------------------------------------------
         # Right: output screen
+        # Consist of
         # ----------------------------------------------------
+
         frameOutput = Frame(parent)
         frameOutput.grid(column = 1, row = 0, rowspan = 2, sticky = E)
 
-        Label(frameOutput, text = 'Output').grid(column = 1, row = 0, sticky = W)
-        outVar = StringVar()
+        Label(frameOutput, text = 'Output').grid(column = 0, row = 2, sticky = W)
+        # this will receive the output from console
         textField = Text(frameOutput)
-        textField.grid(column = 1, row = 1)
+        textField.grid(column = 0, row = 3)
 
-        #TODO: this will receive the output from console
+        Label(frameOutput, text = 'Result').grid(column = 0, row = 0, sticky = W)
+        scrollRes = Scrollbar(frameOutput, orient = VERTICAL)
+        selection = Listbox(frameOutput, yscrollcommand = scrollRes.set, height = 15, width = 107)
+        scrollRes.config(command = selection.yview)
+        scrollRes.grid(column = 1, row = 1, sticky = E)
+        selection.grid(column = 0, row = 1, sticky = W)
 
+        # framePad = Frame(parent)
+        # framePad.grid(column = 0, row = 2)
         # ----------------------------------------------------
         # Bottom-left: set of buttons
         # ----------------------------------------------------
         frameBtn = Frame(parent)
         frameBtn.grid(column = 0, row = 1, sticky = W)
-
-        #TODO: add the related function
-        btnList = Button(frameBtn, text = 'List', command = self.tool.listCompressed(self.pathVar.get()))
 
         btnCompZip = Button(frameBtn, text = 'Zip Compress', command = lambda : self.tool.compressSeparateFolder(self.pathVar.get(), textField))
 
@@ -54,13 +76,9 @@ class myGUI():
         # btnTest    = Button(frameBtn, text = 'Test', command = lambda : self.test(self.pathVar.get(), textField))
         btnTest = Button(frameBtn, text = 'Test', command = lambda: self.threadCall(self.tool.compressSeparateFolder, self.pathVar.get(), textField))
 
-        btnList.pack(side = LEFT) ; btnCompZip.pack(side = LEFT, padx = 10)
-        btnDB.pack(side = LEFT, anchor = 'sw') ; btnTest.pack(side = LEFT, anchor = 'sw')
-        #
-        # # select = Listbox(frameOutput, y)
-        # scroll = Scrollbar(frameOutput, orient = VERTICAL)
-        # # scroll.config(command = sele)
-        # # scroll.(side = RIGHT, fill = Y)
+        # btnCompZip.pack(side = TOP, padx = 10)
+        btnCompZip.grid(column = 0, row = 0, sticky = 'nsew')
+
 
     def showOutput(self, sOutput, textWg):
         textWg.insert(END, sOutput + '\n')
@@ -68,6 +86,11 @@ class myGUI():
 
     def test(self, sOutput, textWg):
         self.showOutput(sOutput, textWg)
+
+    def searchAndShow(self, string):
+        if string in self.db.keys():
+            #TODO: return the  value into appropriate fields
+            pass
 
     def threadCall(self, sFunc, *args):
         # tArg = ()
