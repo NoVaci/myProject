357e89a5e9a3292068b2203d630fc2292e185a38
NoVaci <novaci.merc@gmail.com>
2018-08-28 08:32:11 +0700

    - Add simple continuous conversation

--- ChatBot/ChatterBot/ChatterExample.py
+++ ChatBot/ChatterBot/ChatterExample.py
@@ -3,5 +3,9 @@ from chatterbot import ChatBot
 chatbot = ChatBot('NoVaci', trainer = 'chatterbot.trainers.ChatterBotCorpusTrainer')
 
 # chatbot.train('chatterbot.corpus.vietnamese')
-
-print(chatbot.get_response("lát qua đón"))
+while (True):
+    ask = input('ToKo: ')
+    if ask == 'exit':
+        break
+    else:
+        print("Little Tily: " + str(chatbot.get_response(ask)))
