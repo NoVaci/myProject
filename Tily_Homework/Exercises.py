0ac490addc91616b48dc7aacabf4505debad2519
NoVaci <novaci.merc@gmail.com>
2019-07-17 23:21:45 +0700

    add tily homework

---
+++ Tily_Homework/Exercises.py
@@ -0,0 +1,19 @@
+##############------2 chili-----------#################
+#  Given a list of numbers and calculate the sum ofe  #
+#  all the number can be produced to 10               #
+#######################################################
+
+##############------3 chili-----------#################
+#  Given 2 strings, determines if letters from one    #
+#  appear in the other                                #
+#######################################################
+
+##############------4 chili-----------#################
+#  Given a list of numbers and determines if all the  #
+#  numbers are different from each other              #
+#######################################################
+
+##############------5 chili-----------#################
+#  Given a list of numbers and find the max of the    #
+#  sum of any sequence inside                         #
+#######################################################
