7299413bb69567b5f173c386e976c8d0f08e4d5e
tilytran <tntly26@gmail.com>
2019-07-20 21:57:32 +0700

    - Tily da lam xong bai 2 ot

--- Tily_Homework/Exercises.py
+++ Tily_Homework/Exercises.py
@@ -2,7 +2,11 @@
 #  Given a list of numbers and calculate the sum ofe  #
 #  all the number can be produced to 10               #
 #######################################################
-
+def sum_of_product_10(intList):
+    sum = 0
+    for i in intList:
+        sum += i
+    return sum
 ##############------3 chili-----------#################
 #  Given 2 strings, determines if letters from one    #
 #  appear in the other                                #
@@ -17,3 +21,8 @@
 #  Given a list of numbers and find the max of the    #
 #  sum of any sequence inside                         #
 #######################################################
+
+
+#-------------------------------------------------------
+intList = [2,3,4,5,1,7,8,10]
+print(sum_of_product_10(intList))
